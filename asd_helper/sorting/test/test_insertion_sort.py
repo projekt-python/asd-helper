9389aa29411ce35af8eb8fb10ce1236d394b628d
Przemek Weglik <przemek.weglik@onet.pl>
2022-01-23 16:30:16 +0100

    Added test

--- asd_helper/sorting/test/test_insertion_sort.py
+++ asd_helper/sorting/test/test_insertion_sort.py
@@ -10,8 +10,14 @@ class TestInsertionSort(unittest.TestCase):
 
     def test_simple(self):
         """Test simple sorting."""
-        arr: List[int] = [12, 11, 13, 5, 6, 1]
-        sorted_arr: List[int] = [1, 5, 6, 11, 12, 13]
+        arr: List[int] = [12, 11, 13, 5, 6, 1, 2]
+        sorted_arr: List[int] = [1, 2, 5, 6, 11, 12, 13]
+        self.assertEqual(insertion_sort(arr), sorted_arr)
+
+    def test_another_simple(self):
+        """Test simple sorting."""
+        arr: List[int] = [12, 11, 13, 5, 6, 1, 3]
+        sorted_arr: List[int] = [1, 3, 5, 6, 11, 12, 13]
         self.assertEqual(insertion_sort(arr), sorted_arr)
 
 
